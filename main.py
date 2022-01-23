b10665bf232cab3e83df30c1878217a7e59cf5b7
bellerb <belleroseb@hotmail.com>
2022-01-22 21:58:58 -0500

    update

---
+++ main.py
@@ -0,0 +1,81 @@
+import random
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+
+from bot import Agent
+from wordle import Wordle
+
+ROWS = 6
+LETTERS = 5
+GAMES = 100
+
+w_bank = pd.read_csv('data/words.csv')
+w_bank = w_bank[w_bank['words'].str.len()==LETTERS]
+w_bank['words'] = w_bank['words'].str.upper() #Convert all words to uppercase
+
+control = input('What would you like to do?\n\n-Test Solver [T]\n-Game Assist [A]\n-Play Game [P]\n\n')
+if 'T' in str(control).upper() or 'P' in str(control).upper():
+    if 'P' in str(control).upper():
+        print('PLAY GAME SELECTED\n---------------------\n')
+    else:
+        print('TEST SOLVER SELECTED\n---------------------\n')
+    results = []
+    for _ in tqdm(range(GAMES), desc='GAMES'):
+        word = random.choice(w_bank['words'].tolist())
+        game = Wordle(
+            word,
+            rows=ROWS,
+            letters=LETTERS
+        )
+        bot = Agent(game)
+        while game.is_end() == False:
+            if 'P' in str(control).upper():
+                u_inp = input('\nPLEASE GUESS A 5 LETTER WORD\n')
+            else:
+                u_inp = bot.choose_action()
+            if game.valid_guess(u_inp) == True:
+                game.update_board(u_inp)
+            else:
+                print('ERROR - WORDS MUST BE 5 LETTERS')
+        r = game.game_result()
+        if 'P' in str(control).upper():
+            if r[0] == True:
+                if r[1] > 0:
+                    print(f'\nCONGRATS YOU WON IN {r[1] + 1} GUESSES!\n')
+                else:
+                    print(f'\nCONGRATS YOU WON IN {r[1] + 1} GUESS!\n')
+            else:
+                print(f'\nSORRY YOU DID NOT WIN.\n')
+            print(np.array(game.board))
+        results.append({'word':word,'result':r[0],'moves':r[1]+1})
+
+    results = pd.DataFrame(results)
+    print(results)
+    print(f'Win Percent = {(len(results[results["result"]==True]) / len(results)) * 100}%\nAverage Moves = {results[results["result"]==True]["moves"].mean()}')
+elif 'A' in str(control).upper():
+    print('GAME ASSIST ACTIVATED\n---------------------')
+    game = Wordle(
+        None,
+        rows=ROWS,
+        letters=LETTERS
+    )
+    bot = Agent(game)
+    for i in range(ROWS):
+        guess = bot.choose_action()
+        print(f'\nSuggested Word = {guess}\n')
+        u_inp = input('What were the colours returned [ex. ybggy]?\n')
+        game.colours[i] = [s for s in str(u_inp).upper()]
+        game.board[i] = [s for s in str(guess).upper()]
+        game.g_count += 1
+        for x, s in enumerate(game.colours[i]):
+            if s == 'Y':
+                if guess[x] in bot.y_letters:
+                    bot.y_letters[guess[x]].append(x)
+                else:
+                    bot.y_letters[guess[x]] = [x]
+            elif s == 'B':
+                if guess[x] in bot.g_letters:
+                    bot.g_letters.append(guess[x])
+            elif s == 'G':
+                bot.prediction[x] = guess[x]
--- wordle.py
+++ wordle.py
@@ -1,8 +1,3 @@
-import numpy as np
-import pandas as pd
-
-import random
-
 class Wordle:
     def __init__(self, word, rows=6, letters=5):
         self.g_count = 0
@@ -14,7 +9,7 @@ class Wordle:
         self.alph = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 
     def is_end(self):
-        if self.board[-1] != ['','','','','']:
+        if self.board[-1] != ['' for _ in range(self.letters)]:
             return True
         else:
             r = self.game_result()
@@ -36,11 +31,10 @@ class Wordle:
             self.board[self.g_count][i] = s
             if self.word[i] == s:
                 self.colours[self.g_count][i] = 'G' #Correct place and letter
-            elif s in word:
+            elif s in self.word:
                 self.colours[self.g_count][i] = 'Y' #Wrong place but letter in word
             else:
                 self.colours[self.g_count][i] = 'B' #Letter not in word
-        print(f'\n{"".join(self.board[self.g_count])}\n{self.colours[self.g_count]}')
         self.g_count += 1
 
     def valid_guess(self, u_inp):
@@ -48,112 +42,3 @@ class Wordle:
             return True
         else:
             return False
-
-class Agent:
-    def __init__(self, game):
-        self.vowels = 'A|E|I|O|U|Y'
-        w_bank = pd.read_csv('5L-Words.csv')
-        w_bank['words'] = w_bank['words'].str.upper() #Convert all words to uppercase
-        #w_bank['v-count'] = w_bank['words'].str.count(self.vowels) #Count amount of vowels in words
-
-        #w_bank['w-score'] = [1.] * len(w_bank)
-
-        v_perc = {}
-        for l in ['A','E','I','O','U','Y']:
-            w_bank[f'{l}-count'] = w_bank['words'].str.count(l) #Count amount of vowels in words
-            v_perc[l] = w_bank[f'{l}-count'].sum()/len(w_bank)
-        w_bank['w-score'] = w_bank['A-count'] * v_perc['A']
-        w_bank['w-score'] += w_bank['E-count'] * v_perc['E']
-        w_bank['w-score'] += w_bank['I-count'] * v_perc['I']
-        w_bank['w-score'] += w_bank['O-count'] * v_perc['O']
-        w_bank['w-score'] += w_bank['U-count'] * v_perc['U']
-        w_bank['w-score'] += w_bank['Y-count'] * v_perc['Y']
-
-        l_pos = {}
-        for x in range(5):
-            l_pos[x] = w_bank['words'].str[x].value_counts(normalize=True).to_dict()
-
-        for x in range(5):
-            w_bank['w-score'] *= w_bank['words'].str[x].map(l_pos[x])
-
-        '''
-        l_pos = {}
-        v_pos = {}
-        w_count = len(w_bank)
-        for x in range(5):
-            l_pos[x] = w_bank['words'].str[x].value_counts().to_dict()
-            v_prob = {}
-            for l in ['A','E','I','O','U','Y']:
-                v_prob[l] = l_pos[x][l]
-            v_tot = sum(v_prob.values())
-            v_pos[x] = {k: v / v_tot for k, v in v_prob.items()}
-            l_pos[x] = {k: v / w_count if k not in v_pos[x] else v_pos[x][k] for k, v in l_pos[x].items()}
-
-        for x in range(5):
-            w_bank['w-score'] *= w_bank['words'].str[x].map(l_pos[x])
-        '''
-
-        print(w_bank)
-
-        self.w_bank = w_bank
-        self.game = game
-        self.prediction = ['','','','','']
-        self.y_letters = {}
-        self.g_letters = []
-
-    def parse_board(self):
-        for y, r in enumerate(self.game.colours):
-            if r == ['','','','','']:
-                break
-            else:
-                for x, c in enumerate(r):
-                    letter = self.game.board[y][x]
-                    if c == 'Y':
-                        if letter not in self.y_letters:
-                            self.y_letters[letter] = [x]
-                        else:
-                            self.y_letters[letter].append(x)
-                    elif c == 'G':
-                        self.prediction[x] = letter
-                    else:
-                        if letter not in self.g_letters:
-                            self.g_letters.append(letter)
-
-    def choose_action(self):
-        self.parse_board()
-        if len(self.g_letters) > 0 and len(self.y_letters) > 0:
-            self.w_bank = self.w_bank[(~self.w_bank['words'].str.contains('|'.join(self.g_letters)))&(self.w_bank['words'].str.contains('|'.join(list(self.y_letters.keys()))))]
-            for s, p in self.y_letters.items():
-                for i in p:
-                    self.w_bank = self.w_bank[self.w_bank['words'].str[i]!=s]
-            for i, s in enumerate(self.prediction):
-                if s != '':
-                    self.w_bank = self.w_bank[self.w_bank['words'].str[i]==s]
-            self.y_letters = {}
-            self.g_letters = []
-        mv_bank = self.w_bank[self.w_bank['w-score']==self.w_bank['w-score'].max()] #Find max vowel words
-        result = random.choice(mv_bank['words'].tolist())
-        return result
-
-word = 'HACKS'
-#word = random.choice(w_bank['words'].tolist())
-game = Wordle(word, rows=15)
-bot = Agent(game)
-
-while game.is_end() == False:
-    #u_inp = input('\nPLEASE GUESS A 5 LETTER WORD\n')
-    u_inp = bot.choose_action()
-    if game.valid_guess(u_inp) == True:
-        game.update_board(u_inp)
-    else:
-        print('ERROR - WORDS MUST BE 5 LETTERS')
-
-r = game.game_result()
-if r[0] == True:
-    if r[1] > 0:
-        print(f'\nCONGRATS YOU WON IN {r[1] + 1} GUESSES!\n')
-    else:
-        print(f'\nCONGRATS YOU WON IN {r[1] + 1} GUESS!\n')
-else:
-    print(f'\nSORRY YOU DID NOT WIN.\n')
-print(np.array(game.board))
